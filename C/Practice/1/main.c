1d11475021f64abf7a8a93a71f1eb07a3cda636a
byaxe <byaxe.sx@gmail.com>
2017-09-06 13:31:48 +0300

    master Started solving first exercise

--- C/Practice/1/main.c
+++ C/Practice/1/main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <math.h>
+#include <jmorecfg.h>
 
+/**
+ *
+ * @param x
+ * @param y
+ * @param z
+ * @return
+ */
+double calculate(double x, double y, double z) {
+    double part1, part2, part3, result, tg;
+
+    part1 = pow((8 + pow(fabs(x - y), 2) + 1), 1 / 3);
+    part2 = pow(x, 2) + pow(y, 2) + 2;
+
+    tg = 1 / ((1 + cos(2 * z)) / 2);
+    part3 = exp(fabs(x - y)) * pow(tg + 1, x);
+
+    result = (part1 / part2) - part3;
+
+    return result;
+}
+
+/**
+ *
+ * @return
+ */
+boolean testCalculation() {
+    double x = -4.5;
+    double y = 0.75 * pow(10, -4);
+    double z = 0.854 * pow(10, 2);
+
+    double expected = -55.6848;
+    double actual = calculate(x, y, z);
+
+    printf("Expected: %lf\nActual: %lf\n", expected, actual);
+
+    return expected == actual;
+}
+
+/**
+ *
+ * @return
+ */
 int main() {
-    printf("Hello, World!\n");
+
+//    double x, y, z;
+//    puts("\n\t x,y,z = ");
+//    scanf("%lf %lf %lf", &x, &y, &z);
+
+    boolean result = testCalculation();
+    printf("Test passed: %s", result ? "Yes" : "No");
+
     return 0;
 }
+
