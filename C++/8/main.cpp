9d33dcef82d78ba627a8ae8edad2ad46a28b2ac1
byaxe <byaxe.sx@gmail.com>
2018-04-26 12:49:14 +0300

    master - Started implementation of 8th laboratory work for the second semester

---
+++ C++/8/main.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+typedef double (*uf)(double, int);
+
+void Tabl(double, double, double, uf);
+
+double Y(double, int);
+
+int main() {
+    cout << setw(8) << "x" << setw(15) << "y(x)" << setw(10) << endl;
+    Tabl(0.1, 1, 0.1, Y);
+    cout << endl;
+    return 0;
+}
+
+void Tabl(double a, double b, double h, uf fun) {
+    int k = 0;
+    double sum;
+    for (double x = a; x < b + h / 2; x += h) {
+        sum = fun(x, 20);
+        cout << setw(8) << x << setw(15) << sum << endl;
+    }
+}
+
+double Y(double x, int k) {
+    double s = 2;
+    for (int i = 1; i <= k; i++)
+        s += 2 * pow(x, i) / pow(cos(x), i);
+    return s;
+}
